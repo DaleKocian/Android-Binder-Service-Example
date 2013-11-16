fcc2b064124ecafb9348f56f6c97d2df83e13755
dalekocian <dale.kocian@gmail.com>
2013-11-16 16:57:05 -0600

    Initial working example

---
+++ App/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.app;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.example.app.services.LocalService;
+
+public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int NUMBER_TO_ASK_FOR = 5;
+    private static final String DEFAULT_CASE_HIT_ON_GET_NUMBER = "Default case hit on get number call.";
+    private static final String ERROR_NO_NUM_WAS_ASKED = "Error: we didn't ask for any numbers.";
+    LocalService mService;
+    boolean mBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, LocalService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Called when a button is clicked (there are 2 buttons in the layout file that attaches to
+     * this method with the android:onClick attribute)
+     */
+    public void onGetNumber(View view) {
+        if (mBound) {
+            // Call a method from the LocalService.
+            // However, if this call were something that might hang, then this request should
+            // occur in a separate thread to avoid slowing down the activity performance.
+            String msg;
+            int num;
+            switch (view.getId()) {
+                case R.id.btnNumberGetter:
+                    num = mService.getNumber(NUMBER_TO_ASK_FOR);
+                    msg = "Number Asked For Was " + NUMBER_TO_ASK_FOR + " Number Received Is " + num;
+                    break;
+                case R.id.btnNumberRandom:
+                    num = mService.getRandomNumber();
+                    msg = "Random Number: " + num;
+                    break;
+                default:
+                    msg = ERROR_NO_NUM_WAS_ASKED;
+                    Log.e(TAG, DEFAULT_CASE_HIT_ON_GET_NUMBER);
+                    break;
+            }
+            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void onButtonRandom(View view) {
+        if (mBound) {
+            // Call a method from the LocalService.
+            // However, if this call were something that might hang, then this request should
+            // occur in a separate thread to avoid slowing down the activity performance.
+            int num = mService.getRandomNumber();
+            Toast.makeText(MainActivity.this, "number: " + num, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    // A placeholder fragment containing a simple view.
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+            //Empty
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            return inflater.inflate(R.layout.fragment_main, container, false);
+        }
+    }
+
+    // Defines callbacks for service binding, passed to bindService()
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Unbind from the service
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+}
---
+++ App/src/main/java/com/example/app/services/LocalService.java
@@ -0,0 +1,40 @@
+package com.example.app.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.util.Random;
+
+/**
+ * Created by dkocian on 11/16/13.
+ */
+public class LocalService extends Service {
+    // Binder given to clients
+    private final IBinder mBinder = new LocalBinder();
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    // Method for clients
+    public int getRandomNumber() {
+        return mGenerator.nextInt(100);
+    }
+
+    // Method for clients
+    public int getNumber(int num) {
+        return num;
+    }
+
+    public class LocalBinder extends Binder {
+        public LocalService getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return LocalService.this;
+        }
+    }
+}
